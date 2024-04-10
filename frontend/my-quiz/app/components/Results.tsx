173b52dc459ec744d89b242c4f5c77569cec848c
noor <noordragon2004@gmail.com>
2024-04-09 22:30:47 -0700

    predebloy

--- frontend/my-quiz/app/components/Results.tsx
+++ frontend/my-quiz/app/components/Results.tsx
@@ -8,11 +8,12 @@ import Heading from "./Heading";
 
 const Results = ({ list, answers }: { list: Array<string>; answers: Array<{ answer: number; id: string }> }) => {
   const { questions, isLoading } = useGetQuestions(list);
+  if(isLoading) return <Spinner />
+
   return (
     <>
       <Heading text="Review Your Results Here ..." />
       <section className="grid grid-cols-1 md:grid-cols-2 mt-5 auto-rows-fr gap-4 items-stretch justify-center">
-        {isLoading && <Spinner />}{" "}
         {questions &&
           questions.map((question: QuestionProps, i: number) => (
             <QuestionResults answers={answers} key={i} question={question} />
