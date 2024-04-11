e47639046d927c1ce02f1384916c419f8450cdde
noor <noordragon2004@gmail.com>
2024-04-11 07:02:27 -0700

    meow

--- backend/utils/ApiFeatures.js
+++ backend/utils/ApiFeatures.js
@@ -6,8 +6,11 @@ class ApiFeatures {
         this.queryString = queryString;
     }
     filter() {
-        const queryStr = JSON.stringify(this.queryString);
-        queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+        const queryObj = { ...this.queryString };
+        const excludeFields = ['page', 'sort', 'limit', 'fields'];
+        excludeFields.forEach((el) => delete queryObj[el]);
+        let queryStr = JSON.stringify(queryObj);
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
         this.query = this.query.find(JSON.parse(queryStr));
         return this;
     }
