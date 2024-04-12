c8cecb4fd30b2681d6c1732559f2cbba0e68a3b2
noor <noordragon2004@gmail.com>
2024-04-12 07:59:30 -0700

    version1Edit

--- frontend/my-quiz/actions/SolveQuestion.ts
+++ frontend/my-quiz/actions/SolveQuestion.ts
@@ -6,9 +6,8 @@ import axios from "axios";
 export const SolveQuiz = async (id: string) => {
   try {
     const res = await axios.get(`${API_URL}/quiz/${id}/solve`);
-    const questions=shuffle(res.data.data.quiz.questions)
     const quiz=res.data.data.quiz
-    return {...quiz,questions}
+    return quiz
   } catch (err: any) {
     if (err.response.data) return err.response.data;
     if (err.message === "Failed to fetch")
