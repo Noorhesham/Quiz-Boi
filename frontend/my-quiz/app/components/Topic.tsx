bcb420e27d730c41134c2900197f2ec78b8980f7
noor <noordragon2004@gmail.com>
2024-04-12 05:09:45 -0700

    version1Edit

--- frontend/my-quiz/app/components/Topic.tsx
+++ frontend/my-quiz/app/components/Topic.tsx
@@ -6,14 +6,14 @@ const Topic = ({ tag, small = false }: { tag: string; small?: boolean }) => {
     <Link
       className={` hover:underline hover:text-red-600 
       py-1 px-2 text-base bg-gray-100 text-gray-900 capitalize rounded-lg shadow-sm font-semibold hover:bg-gray-200 duration-200`}
-      href={`/quiz?tags=${tag}`}
+      href={`/?categorey=${tag}`}
     >
       {tag}
     </Link>
   ) : (
     <Link
       className=" hover:underline hover:text-red-600 py-1 px-2 text-sm bg-gray-100 text-gray-900 capitalize rounded-lg shadow-sm font-semibold hover:bg-gray-200 duration-200"
-      href={`/quiz?tags=${tag}`}
+      href={`/?categorey=${tag}`}
     >
       {tag}
     </Link>
