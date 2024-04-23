b4e7bb5dcde0526fb6222fadd8736e9249533d74
noor <noordragon2004@gmail.com>
2024-04-23 06:44:33 -0700

    emew

--- frontend/my-quiz/app/components/Answer.tsx
+++ frontend/my-quiz/app/components/Answer.tsx
@@ -1,20 +1,18 @@
 import React from "react";
-import { useQuiz } from "../context/QuizContext";
 
 const Answer = ({
   answer,
-  setAnswer,
+  setAnswer,active,
   i,
   id,
   text,
 }: {
   answer?: { answer: number | undefined; id: string } | undefined;
-  setAnswer?: any;
+  setAnswer?: any;active:boolean,
   i: number;
   id?: string | undefined;
   text: string;
 }) => {
-  const {answers}=useQuiz()
   return (
     <div
       onClick={() => {
@@ -22,12 +20,12 @@ const Answer = ({
         setAnswer({ answer: i, id });
       }}
       className={`${
-        (answer?.answer===i) ? "bg-red-400 text-gray-100" : "bg-white text-gray-800"
-      } py-2 px-2 md:px-4 flex items-center gap-2 group w-full hover:text-gray-100 hover:bg-red-400 duration-200 cursor-pointer font-semibold  rounded-lg`}
+        (answer?.answer===i) ? "bg-blue-400 text-gray-100":active?"bg-pink-400 text-gray-50" : "bg-white text-gray-800"
+      } py-2 px-2 md:px-4 flex items-center gap-2 group w-full hover:text-gray-100 hover:bg-blue-400 duration-200 cursor-pointer font-semibold  rounded-lg`}
     >
       <div
         className={`${
-          answer?.answer === i ? " text-gray-100" : " text-red-400"
+          (answer?.answer === i||active) ? " text-gray-100" : " text-red-400"
         }  border-r-2  group-hover:text-gray-100 border-gray-300 font-semibold p-2`}
       >
         {i + 1}
--- frontend/my-quiz/app/components/QuestionItem.tsx
+++ frontend/my-quiz/app/components/QuestionItem.tsx
@@ -11,16 +11,23 @@ const QuestionItem = ({ question, len }: { question: QuestionProps; len: number 
   const { handleNext, questionIndex, handleQuizEnd, answers, handlePrev } = useQuiz();
   const [answer, setAnswer] = useState<{ answer: number | undefined; id: string }>();
   const { color } = useColor();
-
   return (
     <div className={`flex flex-col  gap-2  max-w-full lg:w-[80%] rounded-lg py-3 md:py-5  md:px-10`}>
-      {question.coverImage && <img src={question?.coverImage} className="w-[20rem] mx-auto" />}
+      {question?.coverImage && <img src={question?.coverImage} className="w-[20rem] mx-auto" />}
       <div className="bg-white shadow-md   px-5 py-3 md:py-10 md:px-20 relative border-gray-300 border-2 rounded-md">
         <h2 className="  text-base md:text-2xl text-left text-gray-800 font-semibold">{question.question}</h2>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 w-full p-3 gap-4 ">
         {question.answers.map((a, i) => (
-          <Answer key={i} i={i} id={question._id} answer={answer} setAnswer={setAnswer} text={a} />
+          <Answer
+            active={answers.filter((ans:any) => ans.id === question._id)[0]?.answer === i}
+            key={i}
+            i={i}
+            id={question._id}
+            answer={answer}
+            setAnswer={setAnswer}
+            text={a}
+          />
         ))}
       </div>
       {answers.length !== len ? (
