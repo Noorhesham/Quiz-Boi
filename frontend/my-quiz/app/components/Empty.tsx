bc1633e953b949d1a362caa578545258a502d84d
noor <noordragon2004@gmail.com>
2024-04-27 14:02:24 -0700

    second commit

--- frontend/my-quiz/app/components/Empty.tsx
+++ frontend/my-quiz/app/components/Empty.tsx
@@ -3,7 +3,7 @@ import React, { ReactNode } from "react";
 
 export const Empty = ({ text, image, children }: { text: string; image?: string; children?: ReactNode }) => {
   return (
-    <div className=" flex  items-center justify-center p-10 flex-col">
+    <div className=" flex m-auto  items-center justify-center p-10 flex-col">
       <h1 className=" text-gray-800 text-xl md:text-3xl font-semibold">{text}</h1>
       <Image width={300} height={300} src={image ? image : "/play2.png"} alt="asking" />
       {children}
