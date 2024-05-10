0734c4a7aeff6ce740a37a97dbefe64faaf3b799
Noorhesham <noordragon2004@gmail.com>
2024-05-10 17:33:58 +0300

    hige

--- frontend/my-quiz/app/components/Feed.tsx
+++ frontend/my-quiz/app/components/Feed.tsx
@@ -78,7 +78,6 @@ const Feed = ({
         variants={container}
         initial="hidden"
         whileInView="visible"
-        viewport={{ once: true }}
         id="play"
         className=" grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 items-stretch p-10 gap-5"
       >
